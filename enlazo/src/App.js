8795d2b85bd263ab404efa0648dae59b0a6fda28
Andrés Rodríguez <artdavid2022@gmail.com>
2023-09-07 22:20:37 -0500

    Init

---
+++ enlazo/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://i.ytimg.com/vi/6Z0lStsM6pA/maxresdefault.jpg"
+          alt="mantenimiento"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
